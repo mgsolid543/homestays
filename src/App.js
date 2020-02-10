cc5f58ccf8575db0e1617029160bd8a3125f7b8e
Irpan Budiana <irpan.budiana@gmail.com>
2020-02-10 18:04:15 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return(
+      <div className="app">
+        <div className="main">
+          <div className="homestays"></div>
+        </div>
+        <div className="peta"></div>
+      </div>
+    );
+  }
+}
+export default App;
