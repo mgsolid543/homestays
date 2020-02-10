b0f969ebf61540d17982d780033dfab11823fe35
Irpan Budiana <irpan.budiana@gmail.com>
2020-02-10 19:49:52 +0700

    membuat component homestay

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
+import Homestay from './components/Homestay';
 
 class App extends Component {
+  
   render() {
+
+    const homestay = {
+      "nama" : "Lembang Homestay",
+      "fotoUrl" :  "http://bukulokomedia.com/foto/homestay-3.jpg",
+      "harga": 300
+    };
+
     return(
       <div className="app">
         <div className="main">
-          <div className="homestays"></div>
+          <div className="homestays">
+            <Homestay homestay={homestay} />
+            <Homestay homestay={homestay} />
+            <Homestay homestay={homestay} />
+          </div>
         </div>
         <div className="peta"></div>
       </div>
